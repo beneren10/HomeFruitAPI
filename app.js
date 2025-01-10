3f6a9a781a0a522f4102fa347b85f57934e3d348
Ben Eren <ben@bens-air.home>
2025-01-10 23:42:30 +0000

    add comments

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function fruitInput(e) {
     e.preventDefault()
     let val = e.target[0].value
     if (!val){
-        console.log("Please Enter a Value")
+        alert("Please Enter a Value")
     } else {
         getData(val)
     }
@@ -20,8 +20,10 @@ function addFruit(fruit) {
     const li = document.createElement('li')
     const ul = document.createElement('ul')
     const liChild = document.createElement('li')
+
     li.textContent = fruit.name
     liChild.textContent = fruit.family
+    
     fruitList.appendChild(li)
     li.appendChild(ul)
     ul.appendChild(liChild)
